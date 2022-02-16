7f8f031fd5b728fba4eec43b4a66ce1a595e262c
CandyCloud087 <75511982+CandyCloud087@users.noreply.github.com>
2022-02-16 12:56:38 -0300

    shooting game
    Jogo de tiro

---
+++ main.js
@@ -0,0 +1,61 @@
+function enemyAttacksMe(enemy) {
+    enemy.classList.add("showing");
+    setTimeout(() => {
+        enemyShootsMe(enemy);
+    }, 1000);
+    setTimeout(() => {
+        enemy.classList.remove("showing");
+    }, 3000);
+}
+
+function iShoot(enemy) {
+    enemy.classList.add("dead");
+
+    if(!livingEnemies().length) {
+        alert("You win!");
+        window.location.reload();
+    }
+}
+
+function enemyShootsMe(enemy) {
+    if(!enemy.classList.contains("dead")){
+        enemy.classList.add("shooting");
+        updateHealthPoints(healthPoints - 20);
+        setTimeout(() => {
+            enemy.classList.remove("shooting");
+        }, 200);
+    }
+}
+
+function livingEnemies() {
+    return document.querySelectorAll(".enemy:not(.dead");
+}
+
+function randomEnemyAttacks() {
+    var randomEnemyNo = Math.random() * livingEnemies(). length;
+    randomEnemyNo = Math.floor(randomEnemyNo);
+    var enemy = livingEnemies()[randomEnemyNo];
+
+    var randomDelay = Math.random() * 2000 + 1000;
+
+    setTimeout(() => {
+        enemyAttacksMe(enemy);
+        randomEnemyAttacks();
+    }, randomDelay);
+}
+
+var healthPoints = 100;
+
+function updateHealthPoints(points) {
+    healthPoints = points;
+    var healthBar = document.querySelector("#healthBar");
+
+    healthBar.style.width = points + "%";
+
+    if(healthPoints < 1) {
+        alert("Game over!");
+        window.location.reload();
+    }
+}
+
+var gunShot = new Audio("bang.mp3");
